fd876e531c1aeab54bd4fec4f0d942c06938c8ca
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-11-08 04:41:02 +0100

    update fds, change sort argument and add index to output name

--- fds.py
+++ fds.py
@@ -19,18 +19,18 @@ def scrap(tags=None, tag_mode='all', text=None, maximum=500, output_folder="out"
     assert(not (tags is not None and text is not None))
     assert(tag_mode in ['any', 'all'])
     assert(os.path.exists(output_folder))
-    max_digits = len(str(maximum))
+    max_digits = len(str(maximum+1))
 
     flickr = flickrapi.FlickrAPI(config.api_key, config.api_secret)
 
     # https://www.flickr.com/services/api/flickr.photos.search.html
     if tags is not None:
-        photos = flickr.walk(tag_mode='all', tags=tags, per_page=500, license=config.license)
+        photos = flickr.walk(tag_mode='all', tags=tags, per_page=500, sort='relevance', license=config.license)
     else:
-        photos = flickr.walk(text=text, per_page=500, license=config.license)
+        photos = flickr.walk(text=text, per_page=500, sort='relevance', license=config.license)
 
     for index, photo in zip(range(maximum), photos):
-        print(f"{index:0{max_digits}d}", photo.get('title'))
+        print(f"{index+1:0{max_digits}d}", photo.get('title'))
 
         # https://www.flickr.com/services/api/misc.urls.html
         url = f"https://live.staticflickr.com/{photo.get('server')}/{photo.get('id')}_{photo.get('secret')}_{size}.jpg"
@@ -43,7 +43,7 @@ def scrap(tags=None, tag_mode='all', text=None, maximum=500, output_folder="out"
             img.getdata()
             print(url)
             # save image
-            with open(f"{output_folder}/{photo.get('id')}.jpg", 'wb') as file:
+            with open(f"{output_folder}/{index+1:0{max_digits}d}_{photo.get('id')}.jpg", 'wb') as file:
                 file.write(img_data)
         except Exception as e:
             logging.error(traceback.format_exc())
