9e94ee0d887814523b2fbbf595143991de4ce925
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-11-08 07:39:08 +0100

    add argparse for fds

--- fds.py
+++ fds.py
@@ -1,6 +1,7 @@
 import os
 import io
 import time
+import argparse
 import traceback
 import logging
 import hashlib
@@ -41,7 +42,6 @@ def scrap(tags=None, tag_mode='all', text=None, maximum=500, output_folder="out"
             img = Image.open(io.BytesIO(img_data))
             # check for corruption
             img.getdata()
-            print(url)
             # save image
             with open(f"{output_folder}/{index+1:0{max_digits}d}_{photo.get('id')}.jpg", 'wb') as file:
                 file.write(img_data)
@@ -50,3 +50,23 @@ def scrap(tags=None, tag_mode='all', text=None, maximum=500, output_folder="out"
 
         # Limit QPS to 3600 / hour
         time.sleep(1.0)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--text', help='search by text')
+    group.add_argument('--tags', help='search by tags')
+    parser.add_argument('-m', dest='maximum', required=True, help='maximum number of images to download')
+    parser.add_argument('-o', dest='output', required=True, help='output folder')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+
+    print(args)
+
+    if args.tags is not None:
+        scrap(tags=args.tags, tag_mode='all', text=None, maximum=int(args.maximum), output_folder=args.output)
+    else:
+        scrap(tags=None, tag_mode='all', text=args.text, maximum=int(args.maximum), output_folder=args.output)
